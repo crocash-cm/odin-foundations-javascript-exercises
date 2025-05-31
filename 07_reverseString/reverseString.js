b1662571eb1d94433a97f7c44b26d9ae2ffb6928
crocash-cm <jashford.work@gmail.com>
2025-05-31 14:33:54 +0100

    07_reverString complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tempArr = [];
+    let reversedString = "";
+    for (const char of string) {
+        tempArr.unshift(char);
+    }
+    for (const char of tempArr) {
+        reversedString += char;
+    }
+    return reversedString;
 };
 
+let string = "hey";
+console.log(reverseString(string));
 // Do not edit below this line
 module.exports = reverseString;
