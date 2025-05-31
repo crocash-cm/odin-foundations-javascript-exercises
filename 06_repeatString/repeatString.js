d769db6444d6b5c90c41d83bf7f2e76d8aeb6815
crocash-cm <jashford.work@gmail.com>
2025-05-31 14:22:00 +0100

    06_repeatString change return type to string.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,7 @@
 const repeatString = function(string, numTimes) {
-    let repeatedString = [];
+    let repeatedString = "";
     if (numTimes < 0) {
         return "ERROR";
-    }
-    if (numTimes == 0) {
-        return "";
     }
     for (let i = 0; i < numTimes; i++)
     {
