dfe89a34bff8cb716625967f15461566b507745f
crocash-cm <jashford.work@gmail.com>
2025-05-31 14:19:55 +0100

    06_repeatString complete.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    let repeatedString = [];
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+    if (numTimes == 0) {
+        return "";
+    }
+    for (let i = 0; i < numTimes; i++)
+    {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
+
+string = "hey";
+console.log(repeatString(string, 3));
 
 // Do not edit below this line
 module.exports = repeatString;
