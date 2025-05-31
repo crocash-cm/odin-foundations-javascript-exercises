a7a988eaa60c4b3a3da4388c5e50491d3502ea9e
crocash-cm <jashford.work@gmail.com>
2025-05-31 15:27:49 +0100

    08_removeFromArray complete.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let filteredArray = arr;
+    let itemCounts = [];
+    for (const arg of theArgs) {
+        let counter = 0;
+        for (const item of filteredArray) {
+            if (arg === item) {
+                counter++;
+            }
+        }
+        itemCounts.push(counter);
+    }
+    console.log(itemCounts);
+    for (let i = 0; i < theArgs.length; i++) {
+        filteredArray.splice(filteredArray.indexOf(theArgs[i]), itemCounts[i]);
+    }
+    return filteredArray;
 };
+
+let array = [1, 2, 2, 3, 3, 3, 4, 5];
+let filteredArray = removeFromArray(array, 2, 3);
+console.log(filteredArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
