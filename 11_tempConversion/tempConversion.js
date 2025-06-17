b11a86e2e087f2bb45ecb7071239fbd00b18aaf2
crocash-cm <jashford.work@gmail.com>
2025-06-17 16:04:45 +0100

    11_tempConversion.js complete.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Math.round(((f - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Math.round(((c * 1.8) + 32) * 10) / 10;
 };
+
+console.log("32F in C, should be 0: " + convertToCelsius(32));
+console.log("54F in C, should be: 12.222...: " + convertToCelsius(54));
+console.log("0C in F, should be 32: " + convertToFahrenheit(0));
+console.log("21.56C in F, should be 70.808 rounded: " + convertToFahrenheit(21.56));
 
 // Do not edit below this line
 module.exports = {
