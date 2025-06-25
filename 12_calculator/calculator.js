5a4fecf6d9481c825aabd44d516a03a474ed9904
crocash-cm <jashford.work@gmail.com>
2025-06-25 05:17:25 +0100

    12_calculator complete.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product = product * arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0)
+  {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
+
+let answer = factorial(5);
+console.log(answer);
 
 // Do not edit below this line
 module.exports = {
