570fc9bc4034414435336fe7db1230b3a0ebb234
crocash-cm <jashford.work@gmail.com>
2025-07-14 14:37:20 +0100

    15 GetTheTitles complete.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArray) {
+    let titlesArray = [];
+    booksArray.forEach(element => {
+        titlesArray.push(element.title);
+    });
+    return titlesArray;
 };
+
+let testArray = [
+    {
+        title: "book1",
+        author: "name1"
+    },
+    {
+        title: "book2",
+        author: "name2"
+    }
+];
+
+console.log(getTheTitles(testArray));
 
 // Do not edit below this line
 module.exports = getTheTitles;
