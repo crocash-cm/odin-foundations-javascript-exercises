0f24769b4a2af637f5d43f78c9929852d7e7a3c1
crocash-cm <jashford.work@gmail.com>
2025-07-14 15:01:20 +0100

    16 FindTheOldest complete.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,85 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    let greatestAge = 0;
+    let eldestPerson = {};
+    let age = 0;
+    peopleArray.forEach(person => {
+        if (person.yearOfDeath == undefined)
+        {
+            let date = new Date ();
+            let currentYear = date.getFullYear();
+            age = currentYear - person.yearOfBirth;
 
+        }
+        else
+        {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > greatestAge)
+        {
+            greatestAge = age;
+            eldestPerson = person;
+        }
+    });
+    return eldestPerson;
 };
 
+
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+const people1 = [
+    {
+    name: "Carly",
+    yearOfBirth: 2018,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+
+const people2 = [
+    {
+    name: "Carly",
+    yearOfBirth: 1066,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+console.log(people2[0].yearOfDeath);
+console.log(findTheOldest(people).name);
+console.log(findTheOldest(people1).name);
+console.log(findTheOldest(people2).name);
 // Do not edit below this line
 module.exports = findTheOldest;
