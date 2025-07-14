102352c763f31754b9898cfe9484a6a48f17bd7d
crocash-cm <jashford.work@gmail.com>
2025-07-14 14:06:31 +0100

    13_Palindromes complete.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let parsedString = string.toLowerCase(); 
+    parsedString = parsedString.replaceAll(" ", "");
+    parsedString = parsedString.replaceAll(".", "");
+    parsedString = parsedString.replaceAll(",", "");
+    parsedString = parsedString.replaceAll("?", "");
+    parsedString = parsedString.replaceAll("-", "");
+    parsedString = parsedString.replaceAll("!", "");
+    let stringArray = parsedString.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
+    return reversedString === parsedString;
 };
+
+console.log(palindromes("A car, a man, a maraca.")); // true
+console.log(palindromes('racecar')); // true
+console.log(palindromes('tacos')); // false
 
 // Do not edit below this line
 module.exports = palindromes;
