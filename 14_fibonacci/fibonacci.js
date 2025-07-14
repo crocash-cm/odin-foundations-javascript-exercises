d597abb8e2b44e85afaf107d79901529fa6acba8
crocash-cm <jashford.work@gmail.com>
2025-07-14 14:27:09 +0100

    14 Fibonacci complete.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0)
+    {
+        return "OOPS";
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1) 
+    {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
+
+
+console.log(fibonacci(4)); // returns 3
+console.log(fibonacci(6)); // returns 8
 
 // Do not edit below this line
 module.exports = fibonacci;
